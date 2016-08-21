fb2aaa5ffb82c7fb44d11f5e6a89488e84e23f98
Andrew Hobden <andrew@hoverbear.org>
2016-08-21 19:14:21 +0200

    Remove the removed -f flag

--- post.sh
+++ post.sh
@@ -5,5 +5,5 @@ VERSION="$(cat version)"
 
 echo "-----> Testing..."
 docker import archlinux.tar hoverbear/archlinux:$VERSION
-docker tag -f hoverbear/archlinux:$VERSION hoverbear/archlinux:latest
+docker tag hoverbear/archlinux:$VERSION hoverbear/archlinux:latest
 docker run --rm=true hoverbear/archlinux /bin/bash -c "echo \"Success, hoverbear/archlinux prepared.\""
